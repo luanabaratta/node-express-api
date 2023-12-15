a40fcddacf91e9aeeec126c80cb907c247812edc
Luana B. dos Santos <luanabaratta@Luanas-MacBook-Air.local>
2023-12-14 21:14:42 -0300

    starting with routes folder

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-import express from "express";
+import express from 'express';
+import usersRoutes from './routes/users.js';
 
 const app = express();
 const PORT = 4000;
 
 app.use(express.json());
+app.use('/users', usersRoutes);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port: http://localhost:${PORT}`);
-});
+app.get('/', (req, res) => res.send('Hello from Homepage.'));
+
+app.listen(PORT, () => console.log(`Server is running on port: http://localhost:${PORT}`));
