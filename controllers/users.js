1b4e066a6b6b3f5ca9e8ee54d9e95bb5761f955e
Luana B. dos Santos <luanabaratta@Luanas-MacBook-Air.local>
2023-12-15 00:34:57 -0300

    final update

--- controllers/users.js
+++ controllers/users.js
@@ -18,6 +18,12 @@ export const getUserById = (req, res) => {
     res.send(foundUser);
 }
 
+export const deleteUser = (req, res) => {
+    const { id } = req.params;
+    users = users.filter(user => user.id !== id);
+    res.send(`User with the id ${id} deleted from the database`);
+}
+
 export const updateUser = (req, res) => {
     const { id } = req.params;
     const { firstName, lastName, age } = req.body;
@@ -29,10 +35,4 @@ export const updateUser = (req, res) => {
     if(age) user.age = age;
 
     res.send(`User with the id ${id} has been updated`);
-}
-
-export const deleteUser = (req, res) => {
-    const { id } = req.params;
-    users = users.filter(user => user.id !== id);
-    res.send(`User with the id ${id} deleted from the database`);
 }
--- routes/users.js
+++ routes/users.js
@@ -3,15 +3,16 @@ import {
     getUser,
     createUser,
     getUserById,
-    updateUser,
-    deleteUser } from "../controllers/users.js";
+    deleteUser,
+    updateUser
+     } from "../controllers/users.js";
 
 const router = express.Router();
 
 router.get('/', getUser);
 router.post('/', createUser);
 router.get('/:id', getUserById);
-router.patch('/:id', updateUser);
 router.delete('/:id', deleteUser);
+router.patch('/:id', updateUser);
 
 export default router;
