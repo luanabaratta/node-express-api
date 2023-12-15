be4b42198c257c8dba7b0056c1cbb434b7f5b8e0
Luana B. dos Santos <luanabaratta@Luanas-MacBook-Air.local>
2023-12-15 00:31:43 -0300

    creating the controllers folder and file

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ const PORT = 4000;
 app.use(express.json());
 app.use('/users', usersRoutes);
 
-app.get('/', (req, res) => res.send('Hello from Homepage.'));
+app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(PORT, () => console.log(`Server is running on port: http://localhost:${PORT}`));
--- routes/users.js
+++ routes/users.js
@@ -1,31 +1,17 @@
 import express from 'express';
-import { v4 as uuidv4 } from 'uuid';
+import {
+    getUser,
+    createUser,
+    getUserById,
+    updateUser,
+    deleteUser } from "../controllers/users.js";
 
 const router = express.Router();
 
-let users = [];
+router.get('/', getUser);
+router.post('/', createUser);
+router.get('/:id', getUserById);
+router.patch('/:id', updateUser);
+router.delete('/:id', deleteUser);
 
-router.get('/', (req, res) => {
-    res.send(users);
-});
-
-router.post('/', (req, res) => {
-    const user = req.body;
-    users.push({ ...user, id: uuidv4() });
-    res.send(`User with the name: ${user.firstName} added`);
-});
-
-router.get('/:id', (req, res) => {
-    const { id } = req.params;
-    const foundUser = users.find(user => user.id === id );
-    res.send(foundUser);
-});
-
-router.delete('/:id', (req, res) => {
-    const { id } = req.params;
-
-    users = users.filter(user => user.id !== id);
-
-    res.send(`User with the id ${id} deleted from the database`);
-});
 export default router;
