59c8e6fa7024c9db9b7269e141d818cfe6682cfa
Luana B. dos Santos <luanabaratta@Luanas-MacBook-Air.local>
2023-12-14 22:08:26 -0300

    post method has been created

--- routes/users.js
+++ routes/users.js
@@ -2,14 +2,27 @@ import express from 'express';
 
 const router = express.Router();
 
-const users = [{
-    name: "John",
-    lastname: "Doe",
-    age: 25
-}];
+const users = [
+    {
+        firstName: "John",
+        lastName: "Doe",
+        age: 25
+    },
+    {
+        firstName: "Jane",
+        lastName: "Doe",
+        age: 24
+    }
+];
 
 router.get('/', (req, res) => {
-    res.send('Hello');
+    res.send(users);
+});
+
+router.post('/', (req, res) => {
+    const user = req.body;
+    users.push(user);
+    res.send(`User with the name: ${user.firstName} added`);
 });
 
 export default router;
