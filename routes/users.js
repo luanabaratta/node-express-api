e97f93b1320dee3b74f4de6d1c7472925eb56d3e
Luana B. dos Santos <luanabaratta@Luanas-MacBook-Air.local>
2023-12-14 21:18:32 -0300

    adding users

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,12 @@ import express from 'express';
 
 const router = express.Router();
 
+const users = [{
+    name: "John",
+    lastname: "Doe",
+    age: 25
+}];
+
 router.get('/', (req, res) => {
     res.send('Hello');
 });
